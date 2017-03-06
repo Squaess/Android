cca33a9b1a6aa19424f8b59f8c23a42bb1251166
Squaess <babartosz.bns@gmail.com>
2017-03-07 00:47:45 +0100

    Adding new buttons and edning of the game **win

--- Lab1/app/src/main/java/com/example/bartosz/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/bartosz/lab1/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.bartosz.lab1;
 
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Layout;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             {"please","proszę"},
             {"yes","tak"}
     };
+    private boolean[] czyByl;
     private int toRand;
     private String word;
     private String guess;
@@ -34,12 +41,15 @@ public class MainActivity extends AppCompatActivity {
     private int col;
     private int correct=0;
     private int failed=0;
+    private Button check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        check = (Button)findViewById(R.id.b2);
         toRand = dictionary.length;
+        czyByl = new boolean[toRand];
         roll();
     }
 
@@ -47,18 +57,30 @@ public class MainActivity extends AppCompatActivity {
         Random rand =  new Random();
         int prev = row;
         row = rand.nextInt(toRand);
-        if(prev == row) {
-            row = (row+1)%toRand;
+        int val = 0;
+        for(int i = 0; i < toRand; i++) {
+            if(czyByl[i]==true) val++;
         }
-        TextView tv1 = (TextView)findViewById(R.id.cb);
-        tv1.setText("Correct: "+ correct);
-        TextView tv2 = (TextView)findViewById(R.id.fb);
-        tv2.setText("Failed: "+ failed);
-        col = rand.nextInt(2);
-        word = dictionary[row][col];
-        TextView tv = (TextView)findViewById(R.id.word);
-        tv.setText(word);
-        EditText et = (EditText)findViewById(R.id.guess);
+        if(val==toRand) {
+            Toast.makeText(this, "END!!", Toast.LENGTH_SHORT).show();
+            RelativeLayout l = (RelativeLayout)findViewById(R.id.main);
+            l.setBackgroundColor(Color.GREEN);
+        } else {
+            while (prev == row || czyByl[row] == true) {
+                row = (row + 1) % toRand;
+            }
+            col = rand.nextInt(2);
+            word = dictionary[row][col];
+            TextView tv = (TextView) findViewById(R.id.word);
+            tv.setText(word);
+        }
+        check.setEnabled(true);
+        TextView tv1 = (TextView) findViewById(R.id.cb);
+        tv1.setText("Correct: " + correct);
+        TextView tv2 = (TextView) findViewById(R.id.fb);
+        tv2.setText("Failed: " + failed);
+
+        EditText et = (EditText) findViewById(R.id.guess);
         et.setText("");
     }
 
@@ -71,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         guess = et.getText().toString();
         if(guess.equals(dictionary[row][(col+1)%2])) {
             Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show();
+            czyByl[row]=true;
             correct++;
             roll();
         } else {
@@ -83,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
     public void answButton(View view) {
         EditText et = (EditText)findViewById(R.id.guess);
         et.setText(dictionary[row][(col+1)%2]);
+        check.setEnabled(false);
     }
 
     public void fail() {
@@ -92,7 +116,22 @@ public class MainActivity extends AppCompatActivity {
         tv2.setText("Failed: "+ failed);
     }
 
-    public void small() {
+    public void small(View view) {
+        Toast.makeText(this, "Small", Toast.LENGTH_LONG).show();
+        setSize((float)12.0);
+    }
+
+    public void medium(View view) {
+        Toast.makeText(this, "Medium", Toast.LENGTH_LONG).show();
+        setSize((float)16);
+    }
+
+    public void large(View view) {
+        Toast.makeText(this, "Large", Toast.LENGTH_LONG).show();
+        setSize((float)24);
+    }
+
+    private void setSize(float n) {
         TextView heading = (TextView)findViewById(R.id.heading);
         TextView word = (TextView)findViewById(R.id.word);
         EditText gues = (EditText)findViewById(R.id.guess);
@@ -109,9 +148,18 @@ public class MainActivity extends AppCompatActivity {
 
         TextView bot = (TextView)findViewById(R.id.bottom);
 
-        setSize();
+        heading.setTextSize(n);
+        word.setTextSize(n);
+        gues.setTextSize(n);
+        sm.setTextSize(n);
+        me.setTextSize(n);
+        lg.setTextSize(n);
+        an.setTextSize(n);
+        ne.setTextSize(n);
+        ch.setTextSize(n);
+        cb.setTextSize(n);
+        fb.setTextSize(n);
+        bot.setTextSize(n);
     }
-
-    private void setSize() {}
 
 }
