acde901784bb1c2619ac800fb7775b813e818e0e
Squaess <babartosz.bns@gmail.com>
2017-05-27 20:39:08 +0000

    Trying to parse JSON

---
+++ Maps/app/src/main/java/com/example/bartosz/maps/City.java
@@ -0,0 +1,11 @@
+package com.example.bartosz.maps;
+
+/**
+ * Created by aedd on 5/27/17
+ */
+
+public class City {
+    public String name;
+    public double Lat;
+    public double Lng;
+}
--- Maps/app/src/main/java/com/example/bartosz/maps/MapsActivity.java
+++ Maps/app/src/main/java/com/example/bartosz/maps/MapsActivity.java
@@ -6,9 +6,13 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,20 +21,43 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static final String TAG = "MainActivity";
     private GoogleMap mMap;
+
+    // Needed to Snackbar
     private View mLayout;
+
+    // Needed to change permissions
     private static final int REQUEST_FINE_LOCATION = 0;
-    private static final int REQUEST_COARSE = 1;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mLayout = findViewById(R.id.main_layout);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        Gson gson = new Gson();
+
+        City[] cities = gson.fromJson(, City[].class);
+        ArrayList<String> miasta = new ArrayList<>();
+        for (City c : cities){
+            miasta.add(c.name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, miasta);
+        spinner.setAdapter(adapter);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -38,6 +65,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     void addActGpsLocation() {
+
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -59,6 +87,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(wroclaw).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wroclaw, 13));
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -68,11 +97,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        double myLat = 51.116;
-        double myLng = 17.06;
+        mMap.setOnMapLoadedCallback(this);
+
+        double myLat = -33.865143;
+        double myLng = 151.209900;
 
         // Get location form GPS system
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -86,8 +118,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions java.lang.Stringfor more details.
+
+            // Jesli nie mamy dostepu to prosimy uzytkownika o dostep
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
         } else {
+
+            // In case we have permission just make marker and move
             Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
             if (loc == null) {
@@ -108,18 +144,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapLoaded() {
-        Toast.makeText(this, "Map loaded", Toast.LENGTH_SHORT).show();
+        Snackbar.make(mLayout, R.string.READY,
+                Snackbar.LENGTH_SHORT)
+                .show();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_FINE_LOCATION: {
                 if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     // we should move camera and add marker on gps location
                     addActGpsLocation();
+                } else {
+                    setDefaultLocation();
                 }
             }
         }
+    }
+
+    private void setDefaultLocation() {
+
+        double myLat = 52.237049;
+        double myLng = 21.017532;
+
+        // Add a marker in Wroclaw and move the camera
+        LatLng wroclaw = new LatLng(myLat, myLng);
+        mMap.addMarker(new MarkerOptions().position(wroclaw).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wroclaw, 13));
     }
 }
